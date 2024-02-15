84f2f2bf11289555abab093dd75b997e26a616e1
Alexis GONNEAUD <alexis.gonneaud@hotmail.fr>
2024-02-15 16:46:16 +0100

    Remove : removed default page

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:econo_me/main.dart';
+// import 'package:econo_me/main.dart';
+import 'package:econo_me/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
